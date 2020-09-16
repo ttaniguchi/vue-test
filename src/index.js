4d7356094d598638c6b0da20327b1be641752ed6
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2020-09-16 12:02:23 +0900

    init vuejs build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, World!');
+import Vue from 'vue';
+import App from './components/App';
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<app/>',
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = (env, { mode }) => ({
   entry: './src/index.js',
@@ -6,7 +7,24 @@ module.exports = (env, { mode }) => ({
     path: path.resolve(__dirname, './dest'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ]
+  },
   devServer: {
     contentBase: path.resolve(__dirname, 'public')
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 });
